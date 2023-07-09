a20fee3fb167e352d6a7856d8ca9786fc2361b89
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-07-08 23:48:59 -0600

    [Fix]: Migration updated

--- Notes_Management_API/api/migrations/0001_initial.py
+++ Notes_Management_API/api/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             name='Note',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=60)),
-                ('description', models.CharField(max_length=255)),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=1000)),
             ],
         ),
     ]
